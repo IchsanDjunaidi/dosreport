49a228fb6d7dbbfc3959ba53ba3e15ef7c61e0a7
IchsanDjunaidi <30589000+IchsanDjunaidi@users.noreply.github.com>
2017-07-31 15:06:09 +0700

    Add files via upload

---
+++ LoginRequest.java
@@ -0,0 +1,30 @@
+package com.dosindonesia.www.dosindoreport;
+
+        import com.android.volley.Request;
+        import com.android.volley.Response;
+        import com.android.volley.toolbox.StringRequest;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+/**
+ * Created by IT on 3/9/2017.
+ */
+
+public class LoginRequest extends StringRequest {
+
+    private static final String LOGIN_REQUEST_URL ="http://www.dosindonesia.com/android/loginpc.php";
+    private Map<String, String> params;
+    public LoginRequest (String username, String password, Response.Listener<String>listener){
+        super(Request.Method.POST,LOGIN_REQUEST_URL, listener, null);
+
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+}
